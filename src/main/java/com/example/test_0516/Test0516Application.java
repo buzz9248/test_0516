dacd074c30521653b2e83e228e0c7ebc74d2f8dc
buzz9248 <buzz9248@naver.com>
2025-05-16 09:27:51 +0900

    first commit

---
+++ src/main/java/com/example/test_0516/Test0516Application.java
@@ -0,0 +1,13 @@
+package com.example.test_0516;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test0516Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test0516Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test_0516/Test0516ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_0516;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test0516ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
